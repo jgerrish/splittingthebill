254a657a83e2b45bf6f9a6bf80fdca5c7fcd19b6
Joshua Gerrish <jgerrish@gmail.com>
2014-03-05 14:56:59 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Splittingthebill::Application.config.secret_key_base = 'e833afdc7e3d05c7bc33729227e49466a868c4a1eb4dc3fd93a7be274de14b1aaa078858612420454f83e1797a45ba33773133840b03b6ceda4335347c0a3e05'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Splittingthebill::Application.config.session_store :cookie_store, key: '_splittingthebill_session'
